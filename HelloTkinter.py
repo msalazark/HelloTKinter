2e977208af05c6376346283539a40eb8a3d750e5
Miguel Salazar <msalazark@gmail.com>
2024-04-28 13:49:43 -0500

    Init

---
+++ HelloTkinter.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.hi_there = Button(frame, text="Hola", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+    def say_hi(self):
+        print("hola todo el mundo!")
+
+root = Tk()
+app = App(root)
+root.mainloop()
